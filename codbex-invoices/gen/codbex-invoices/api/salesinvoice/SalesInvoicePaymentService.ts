8471f4d655bfaa684d9eee3c9b02eb289f155334
MinaDoncheva <mina.doncheva@icloud.com>
2024-06-27 13:43:29 +0300

    regenerate from edm

---
+++ codbex-invoices/gen/codbex-invoices/api/salesinvoice/SalesInvoicePaymentService.ts
@@ -0,0 +1,144 @@
+import { Controller, Get, Post, Put, Delete, response } from "sdk/http"
+import { Extensions } from "sdk/extensions"
+import { SalesInvoicePaymentRepository, SalesInvoicePaymentEntityOptions } from "../../dao/salesinvoice/SalesInvoicePaymentRepository";
+import { ValidationError } from "../utils/ValidationError";
+import { HttpUtils } from "../utils/HttpUtils";
+
+const validationModules = await Extensions.loadExtensionModules("codbex-invoices-salesinvoice-SalesInvoicePayment", ["validate"]);
+
+@Controller
+class SalesInvoicePaymentService {
+
+    private readonly repository = new SalesInvoicePaymentRepository();
+
+    @Get("/")
+    public getAll(_: any, ctx: any) {
+        try {
+            const options: SalesInvoicePaymentEntityOptions = {
+                $limit: ctx.queryParameters["$limit"] ? parseInt(ctx.queryParameters["$limit"]) : undefined,
+                $offset: ctx.queryParameters["$offset"] ? parseInt(ctx.queryParameters["$offset"]) : undefined
+            };
+
+            let SalesInvoice = parseInt(ctx.queryParameters.SalesInvoice);
+            SalesInvoice = isNaN(SalesInvoice) ? ctx.queryParameters.SalesInvoice : SalesInvoice;
+
+            if (SalesInvoice !== undefined) {
+                options.$filter = {
+                    equals: {
+                        SalesInvoice: SalesInvoice
+                    }
+                };
+            }
+
+            return this.repository.findAll(options);
+        } catch (error: any) {
+            this.handleError(error);
+        }
+    }
+
+    @Post("/")
+    public create(entity: any) {
+        try {
+            this.validateEntity(entity);
+            entity.Id = this.repository.create(entity);
+            response.setHeader("Content-Location", "/services/ts/codbex-invoices/gen/codbex-invoices/api/salesinvoice/SalesInvoicePaymentService.ts/" + entity.Id);
+            response.setStatus(response.CREATED);
+            return entity;
+        } catch (error: any) {
+            this.handleError(error);
+        }
+    }
+
+    @Get("/count")
+    public count() {
+        try {
+            return this.repository.count();
+        } catch (error: any) {
+            this.handleError(error);
+        }
+    }
+
+    @Post("/count")
+    public countWithFilter(filter: any) {
+        try {
+            return this.repository.count(filter);
+        } catch (error: any) {
+            this.handleError(error);
+        }
+    }
+
+    @Post("/search")
+    public search(filter: any) {
+        try {
+            return this.repository.findAll(filter);
+        } catch (error: any) {
+            this.handleError(error);
+        }
+    }
+
+    @Get("/:id")
+    public getById(_: any, ctx: any) {
+        try {
+            const id = parseInt(ctx.pathParameters.id);
+            const entity = this.repository.findById(id);
+            if (entity) {
+                return entity;
+            } else {
+                HttpUtils.sendResponseNotFound("SalesInvoicePayment not found");
+            }
+        } catch (error: any) {
+            this.handleError(error);
+        }
+    }
+
+    @Put("/:id")
+    public update(entity: any, ctx: any) {
+        try {
+            entity.Id = ctx.pathParameters.id;
+            this.validateEntity(entity);
+            this.repository.update(entity);
+            return entity;
+        } catch (error: any) {
+            this.handleError(error);
+        }
+    }
+
+    @Delete("/:id")
+    public deleteById(_: any, ctx: any) {
+        try {
+            const id = ctx.pathParameters.id;
+            const entity = this.repository.findById(id);
+            if (entity) {
+                this.repository.deleteById(id);
+                HttpUtils.sendResponseNoContent();
+            } else {
+                HttpUtils.sendResponseNotFound("SalesInvoicePayment not found");
+            }
+        } catch (error: any) {
+            this.handleError(error);
+        }
+    }
+
+    private handleError(error: any) {
+        if (error.name === "ForbiddenError") {
+            HttpUtils.sendForbiddenRequest(error.message);
+        } else if (error.name === "ValidationError") {
+            HttpUtils.sendResponseBadRequest(error.message);
+        } else {
+            HttpUtils.sendInternalServerError(error.message);
+        }
+    }
+
+    private validateEntity(entity: any): void {
+        if (entity.CustomerPayment === null || entity.CustomerPayment === undefined) {
+            throw new ValidationError(`The 'CustomerPayment' property is required, provide a valid value`);
+        }
+        if (entity.Amount === null || entity.Amount === undefined) {
+            throw new ValidationError(`The 'Amount' property is required, provide a valid value`);
+        }
+        for (const next of validationModules) {
+            next.validate(entity);
+        }
+    }
+
+}
---
+++ codbex-invoices/gen/codbex-invoices/ui/purchaseinvoice/PurchaseInvoice/PurchaseInvoiceItem/dialog-window/controller.js
@@ -0,0 +1,67 @@
+angular.module('page', ["ideUI", "ideView", "entityApi"])
+	.config(["messageHubProvider", function (messageHubProvider) {
+		messageHubProvider.eventIdPrefix = 'codbex-invoices.purchaseinvoice.PurchaseInvoiceItem';
+	}])
+	.config(["entityApiProvider", function (entityApiProvider) {
+		entityApiProvider.baseUrl = "/services/ts/codbex-invoices/gen/codbex-invoices/api/purchaseinvoice/PurchaseInvoiceItemService.ts";
+	}])
+	.controller('PageController', ['$scope', 'messageHub', 'ViewParameters', 'entityApi', function ($scope, messageHub, ViewParameters, entityApi) {
+
+		$scope.entity = {};
+		$scope.forms = {
+			details: {},
+		};
+		$scope.formHeaders = {
+			select: "PurchaseInvoiceItem Details",
+			create: "Create PurchaseInvoiceItem",
+			update: "Update PurchaseInvoiceItem"
+		};
+		$scope.action = 'select';
+
+		let params = ViewParameters.get();
+		if (Object.keys(params).length) {
+			$scope.action = params.action;
+			$scope.entity = params.entity;
+			$scope.selectedMainEntityKey = params.selectedMainEntityKey;
+			$scope.selectedMainEntityId = params.selectedMainEntityId;
+			$scope.optionsPurchaseInvoice = params.optionsPurchaseInvoice;
+			$scope.optionsProduct = params.optionsProduct;
+			$scope.optionsUoM = params.optionsUoM;
+		}
+
+		$scope.create = function () {
+			let entity = $scope.entity;
+			entity[$scope.selectedMainEntityKey] = $scope.selectedMainEntityId;
+			entityApi.create(entity).then(function (response) {
+				if (response.status != 201) {
+					messageHub.showAlertError("PurchaseInvoiceItem", `Unable to create PurchaseInvoiceItem: '${response.message}'`);
+					return;
+				}
+				messageHub.postMessage("entityCreated", response.data);
+				$scope.cancel();
+				messageHub.showAlertSuccess("PurchaseInvoiceItem", "PurchaseInvoiceItem successfully created");
+			});
+		};
+
+		$scope.update = function () {
+			let id = $scope.entity.Id;
+			let entity = $scope.entity;
+			entity[$scope.selectedMainEntityKey] = $scope.selectedMainEntityId;
+			entityApi.update(id, entity).then(function (response) {
+				if (response.status != 200) {
+					messageHub.showAlertError("PurchaseInvoiceItem", `Unable to update PurchaseInvoiceItem: '${response.message}'`);
+					return;
+				}
+				messageHub.postMessage("entityUpdated", response.data);
+				$scope.cancel();
+				messageHub.showAlertSuccess("PurchaseInvoiceItem", "PurchaseInvoiceItem successfully updated");
+			});
+		};
+
+		$scope.cancel = function () {
+			$scope.entity = {};
+			$scope.action = 'select';
+			messageHub.closeDialogWindow("PurchaseInvoiceItem-details");
+		};
+
+	}]);
