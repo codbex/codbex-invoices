a9b6c79295a1cec242bf00ad044b0aeb27dc68a0
MinaDoncheva <mina.doncheva@icloud.com>
2024-12-20 11:26:46 +0200

    adds Cashflow Report

---
+++ codbex-invoices/gen/cashflow/ui/Reports/CASHFLOW/dialog-window/controller.js
@@ -0,0 +1,13 @@
+angular.module('page', ["ideUI", "ideView"])
+	.controller('PageController', ['$scope', 'ViewParameters', function ($scope, ViewParameters) {
+
+		$scope.entity = {};
+
+		let params = ViewParameters.get();
+		if (Object.keys(params).length) {
+			$scope.action = "select";;
+
+			$scope.entity = params.entity;
+		}
+
+	}]);
