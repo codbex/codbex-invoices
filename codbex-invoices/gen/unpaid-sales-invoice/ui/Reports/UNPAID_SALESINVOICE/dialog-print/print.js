eaceb52c5262e324e9f55d40f68f62d35fb6601d
Nikol Georgieva <nicole.georgieva18@abv.bg>
2025-02-18 17:21:06 +0200

    Fix report columns naming (#64)

---
+++ codbex-invoices/gen/unpaid-sales-invoice/ui/Reports/UNPAID_SALESINVOICE/dialog-print/print.js
@@ -0,0 +1,15 @@
+const viewData = {
+    id: 'codbex-invoices-Reports-UNPAID_SALESINVOICE-print',
+    label: 'Print',
+    link: '/services/web/codbex-invoices/gen/unpaid-sales-invoice/ui/Reports/UNPAID_SALESINVOICE/dialog-print/index.html',
+    perspective: 'Reports',
+    view: 'UNPAID_SALESINVOICE',
+    type: 'page',
+    order: 10
+};
+
+if (typeof exports !== 'undefined') {
+    exports.getDialogWindow = function () {
+        return viewData;
+    }
+}
