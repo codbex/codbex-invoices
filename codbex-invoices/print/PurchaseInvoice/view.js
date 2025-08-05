255f2f6f79ed166c3aef72ce89fb7761f7943076
NicoleNG18 <nicole.georgieva18@abv.bg>
2025-08-05 18:12:14 +0300

    migrate print templates

--- codbex-invoices/print/PurchaseInvoice/view.js
+++ codbex-invoices/print/PurchaseInvoice/view.js
@@ -2,11 +2,15 @@ const viewData = {
     id: 'purchase-invoice-print',
     label: 'Print',
     path: '/services/ts/codbex-templates/print/purchase-invoice-print-template.ts',
-    perspective: 'purchaseinvoice',
+    translation: {
+        key: 'codbex-orders:t.PURCHASEINVOICE',
+    },
+    perspective: 'PurchaseInvoice',
     view: 'PurchaseInvoice',
     type: 'entity',
     order: 30
 };
+
 if (typeof exports !== 'undefined') {
     exports.getView = () => viewData;
 }
