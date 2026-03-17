bf7ac5015fd04c7b6c2fe1e861255e93b686405d
MinaDoncheva <133380007+MinaDoncheva@users.noreply.github.com>
2026-03-17 09:24:20 +0200

    Refactor numbers to be generated by type (#88)

--- codbex-invoices/gen/codbex-invoices/data/SalesInvoice/SalesInvoiceRepository.ts
+++ codbex-invoices/gen/codbex-invoices/data/SalesInvoice/SalesInvoiceRepository.ts
@@ -1,4 +1,4 @@
-import { Repository, EntityEvent, EntityConstructor } from '@aerokit/sdk/db'
+import { Repository, EntityEvent, EntityConstructor, Options } from '@aerokit/sdk/db'
 import { Component } from '@aerokit/sdk/component'
 import { Producer } from '@aerokit/sdk/messaging'
 import { Extensions } from '@aerokit/sdk/extensions'
@@ -13,15 +13,33 @@ export class SalesInvoiceRepository extends Repository<SalesInvoiceEntity> {
         super((SalesInvoiceEntity as EntityConstructor));
     }
 
+    public override findById(id: string | number, options?: Options): SalesInvoiceEntity | undefined {
+        const entity = super.findById(id, options);
+        if (entity) {
+            entity.Date = entity.Date ? new Date(entity.Date) : undefined;
+            entity.Due = entity.Due ? new Date(entity.Due) : undefined;
+        }
+        return entity;
+    }
+
+    public override findAll(options?: Options): SalesInvoiceEntity[] {
+        const entities = super.findAll(options);
+        entities.forEach(entity => {
+            entity.Date = entity.Date ? new Date(entity.Date) : undefined;
+            entity.Due = entity.Due ? new Date(entity.Due) : undefined;
+        });
+        return entities;
+    }
+
     public override create(entity: SalesInvoiceEntity): string | number {
-        entity.Number = new NumberGeneratorService().generate(5);
+        entity.Number = new NumberGeneratorService().generateByType('Sales Invoice');
         entity.Name = entity["Number"] + "/" + new Date(entity["Date"]).toISOString().slice(0, 10) + "/" + entity["Total"];
         entity.UUID = require("sdk/utils/uuid").random();
         return super.create(entity);
     }
 
     public override upsert(entity: SalesInvoiceEntity): string | number {
-        entity.Number = new NumberGeneratorService().generate(5);
+        entity.Number = new NumberGeneratorService().generateByType('Sales Invoice');
         entity.Name = entity["Number"] + "/" + new Date(entity["Date"]).toISOString().slice(0, 10) + "/" + entity["Total"];
         entity.UUID = require("sdk/utils/uuid").random();
         entity.Name = entity["Number"] + "/" + new Date(entity["Date"]).toISOString().slice(0, 10) + "/" + entity["Total"];
