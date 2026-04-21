a9f06f77b99006b01ab1fe7fd937844b484250e5
Nikol Georgieva <nicole.georgieva18@abv.bg>
2026-04-21 15:41:43 +0300

    Make number field in sales and purchase invoice not required (#100)

--- codbex-invoices/gen/codbex-invoices/api/SalesInvoice/SalesInvoiceController.ts
+++ codbex-invoices/gen/codbex-invoices/api/SalesInvoice/SalesInvoiceController.ts
@@ -167,9 +167,6 @@ class SalesInvoiceController {
     }
 
     private validateEntity(entity: any): void {
-        if (entity.Number === null || entity.Number === undefined) {
-            throw new ValidationError(`The 'Number' property is required, provide a valid value`);
-        }
         if (entity.Number?.length > 20) {
             throw new ValidationError(`The 'Number' exceeds the maximum length of [20] characters`);
         }
